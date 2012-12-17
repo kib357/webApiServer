f584b31ec3f8ce981e507213d49ac7bd8ca9c64a
sv1809 <sv1809@gmail.com>
2012-12-17 16:07:22 +0600

    гит очень просил

--- WPFBacNetApiSample/MyViewModel.cs
+++ WPFBacNetApiSample/MyViewModel.cs
@@ -89,7 +89,7 @@ namespace WPFBacNetApiSample
         private void OnNetworkModelChanged()
         {
             Devices = new ObservableCollection<BacNetDevice>(Bacnet.OnlineDevices);
-            IamCount = _bacnet.IamCount;
+            IamCount = Bacnet.IamCount;
         }
 
         private ObservableCollection<BacNetDevice> _devices;
